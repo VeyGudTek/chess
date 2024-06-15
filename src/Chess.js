d0b64ba02c6ce0d060ea5910bf6a5bc5a336201e
Tim Lin <lintim03@gmail.com>
2024-06-14 19:13:48 -0500

    Added rest of pieces to board

--- src/Chess.js
+++ src/Chess.js
@@ -20,9 +20,30 @@ export default class Chess{
         }
 
         for (let i = 0; i < 8; i++){
-            board[1][i].piece = new Pawn('black'), [1, i], this
-            board[6][i].piece = new Pawn('white'), [6, i], this
+            board[1][i].piece = new Pawn('black', [1, i], this)
+            board[6][i].piece = new Pawn('white', [6, i], this)
         }
+
+        board[0][0].piece = new Rook('black', [0, 0], this)
+        board[0][7].piece = new Rook('black', [0, 7], this)
+        board[7][0].piece = new Rook('white', [7, 0], this)
+        board[7][7].piece = new Rook('white', [7, 7], this)
+
+        board[0][1].piece = new Knight('black', [0, 1], this)
+        board[0][6].piece = new Knight('black', [0, 6], this)
+        board[7][1].piece = new Knight('white', [7, 1], this)
+        board[7][6].piece = new Knight('white', [7, 6], this)
+
+        board[0][2].piece = new Bishop('black', [0, 2], this)
+        board[0][5].piece = new Bishop('black', [0, 5], this)
+        board[7][2].piece = new Bishop('white', [7, 2], this)
+        board[7][5].piece = new Bishop('white', [7, 5], this)
+        
+        board[0][3].piece = new King('black', [0, 3], this)
+        board[0][4].piece = new Queen('black', [0, 4], this)
+        board[7][3].piece = new King('white', [7, 3], this)
+        board[7][4].piece = new Queen('white', [7, 4], this)
+
 
         return board
     }
