8c512590c654a00444996d5bca3b2cfdc3aa4446
Tim Lin <lintim03@gmail.com>
2024-07-13 15:01:54 -0500

    Flip Board to reflect turn

--- src/Board.jsx
+++ src/Board.jsx
@@ -3,26 +3,33 @@ import selected_png from "./assets/selected.png"
 import './board.css'
 
 const Board = ({chess, handleClick, selected}) => {
+    const css_coord = (coord) => {
+        if (chess.turn === "white"){
+            return coord + 1
+        }else{
+            return 8 - coord
+        }
+    }
 
     return (
         <>
             <div className="chess-board">
                 {[0, 1, 2, 3, 4, 5, 6, 7].map((row) => (
                     [0, 1, 2, 3, 4, 5, 6, 7].map((col) => (
-                        <div className="square" style={{gridColumn: col + 1, gridRow: row + 1}} key={'square' + row.toString() + col.toString()}>
+                        <div className="square" style={{gridColumn: css_coord(col), gridRow: css_coord(row)}} key={'square' + row.toString() + col.toString()}>
                             <img src={chess.board[row][col].get_image()} onClick={() => {handleClick([row, col])}}/>
                         </div>
                     ))
                 ))}
 
-                {selected && <div className="square" style={{gridColumn: selected.coordinates[1] + 1, gridRow: selected.coordinates[0] + 1}}>
+                {selected && <div className="square" style={{gridColumn: css_coord(selected.coordinates[1]), gridRow: css_coord(selected.coordinates[0])}}>
                     <img src={selected_png} onClick={() => {handleClick([selected.coordinates[0], selected.coordinates[1]])}}/>
                 </div>}
 
                 {[0, 1, 2, 3, 4, 5, 6, 7].map((row) => (
                     [0, 1, 2, 3, 4, 5, 6, 7].map((col) => (
                         chess.board[row][col].piece !== null
-                         ? (<div className="square" style={{gridColumn: col + 1, gridRow: row + 1}} key={'piece' + row.toString() + col.toString()}>
+                         ? (<div className="square" style={{gridColumn: css_coord(col), gridRow: css_coord(row)}} key={'piece' + row.toString() + col.toString()}>
                             <img src={chess.board[row][col].piece.get_image()} onClick={() => {handleClick([row, col], chess.board[row][col].piece)}}/>
                          </div>) 
                          : null
@@ -30,7 +37,7 @@ const Board = ({chess, handleClick, selected}) => {
                 ))}
 
                 {selected && selected.get_moves().map((coordinates) => (
-                    <div className="square" style={{gridColumn: coordinates[1] + 1, gridRow: coordinates[0] + 1}} key={'move' + coordinates[0].toString() + coordinates[1].toString()}> 
+                    <div className="square" style={{gridColumn: css_coord(coordinates[1]), gridRow: css_coord(coordinates[0])}} key={'move' + coordinates[0].toString() + coordinates[1].toString()}> 
                         <img src={hit} onClick={() => {handleClick([coordinates[0], coordinates[1]])}}/>
                     </div>
                 ))}
